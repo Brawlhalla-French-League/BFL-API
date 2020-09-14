8de03d2b1382b1e29d3b23231b0ab8a44c2a1c0b
Djobbo "Alfie" Maïga <djobbo.maiga@gmail.com>
2020-09-14 19:28:08 +0200

    function fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
-import * as functions from "firebase-functions";
-import * as express from "express";
-import { createClient } from "contentful";
-import { Entry, EntryFields, Asset } from "contentful";
-import { Document } from "@contentful/rich-text-types";
-import * as cors from "cors";
+import * as functions from 'firebase-functions';
+import * as express from 'express';
+import { createClient } from 'contentful';
+import { Entry, EntryFields, Asset } from 'contentful';
+import { Document } from '@contentful/rich-text-types';
+import * as cors from 'cors';
 
 const app = express();
 const router = express.Router();
@@ -13,81 +13,85 @@ app.use(cors({ origin: true }));
 const { space_id, access_token } = functions.config().contentful;
 
 const client = createClient({
-  space: space_id,
-  accessToken: access_token,
+	space: space_id,
+	accessToken: access_token,
 });
 
 interface IStructure {
-  name: EntryFields.Text;
-  content: Document;
-  logo: Asset;
+	name: EntryFields.Text;
+	content: Document;
+	logo: Asset;
 }
 
 export type StructureEntry = Entry<IStructure>;
 
 export const getStructures = (): Promise<StructureEntry[]> =>
-  client
-    .getEntries<IStructure>({
-      content_type: "bflStructure",
-      order: "fields.name",
-    })
-    .then((res) => res.items);
+	client
+		.getEntries<IStructure>({
+			content_type: 'bflStructure',
+			order: 'fields.name',
+		})
+		.then((res) => res.items);
 
 interface IBlogPost {
-  title: EntryFields.Text;
-  slug: EntryFields.Text;
-  thumbnail: Asset;
-  excerpt: EntryFields.Text;
-  content: Document;
-  date: EntryFields.Date;
-  author: EntryFields.Text;
+	title: EntryFields.Text;
+	slug: EntryFields.Text;
+	thumbnail: Asset;
+	excerpt: EntryFields.Text;
+	content: Document;
+	date: EntryFields.Date;
+	author: EntryFields.Text;
 }
 
 export type BlogPostEntry = Entry<IBlogPost>;
 
 export const getBlogPosts = (): Promise<BlogPostEntry[]> =>
-  client
-    .getEntries<IBlogPost>({
-      content_type: "bflBlogPost",
-    })
-    .then((res) => res.items);
+	client
+		.getEntries<IBlogPost>({
+			content_type: 'bflBlogPost',
+		})
+		.then((res) => res.items);
 
 export const getBlogPost = (slug: string): Promise<BlogPostEntry> =>
-  client
-    .getEntries<IBlogPost>({
-      "fields.slug": slug,
-      content_type: "bflBlogPost",
-    })
-    .then((res) => res.items[0]);
+	client
+		.getEntries<IBlogPost>({
+			'fields.slug': slug,
+			content_type: 'bflBlogPost',
+		})
+		.then((res) => res.items[0]);
 
-router.get("/", (req, res) => {
-  res.status(200).send("BFL API");
+router.get('/', (req, res) => {
+	res.status(200).send('BFL API');
 });
 
-router.get("/blog", (req, res) => {
-  getBlogPosts().then((data) => {
-    res.status(200).send(data);
-  });
+router.get('/blog', (req, res) => {
+	getBlogPosts().then((data) => {
+		res.status(200).send(data);
+	});
 });
 
-router.get("/blog/:slug", (req, res) => {
-  const { slug } = req.params;
+router.get('/blog/:slug', (req, res) => {
+	const { slug } = req.params;
 
-  getBlogPost(slug).then((data) => {
-    res.status(200).send(data);
-  });
+	getBlogPost(slug).then((data) => {
+		res.status(200).send(data);
+	});
 });
 
-router.get("/structures", (req, res) => {
-  getStructures().then((data) => {
-    res.status(200).send(data);
-  });
+router.get('/structures', (req, res) => {
+	getStructures().then((data) => {
+		res.status(200).send(data);
+	});
 });
 
-router.get("/**", (req, res) => {
-  res.status(404).send("404 Not Found!");
+router.get('/**', (req, res) => {
+	res.status(404).send('404 Not Found!');
 });
 
-app.get("/api", router);
+router.get('**', (req, res) => {
+	res.status(404).send(JSON.stringify(req.params));
+});
 
-export const api = functions.region("europe-west1").https.onRequest(app);
+app.get('/', router);
+
+export const api = functions.https.onRequest(app);
--- makefile
+++ makefile
@@ -1,5 +1,8 @@
 make:
 	firebase deploy --only functions
 
+emulate:
+	cd functions && npm run build && cd .. && firebase emulators:start --only functions
+
 local-env:
 	cd functions && firebase functions:config:get > .runtimeconfig.json
